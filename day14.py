a5debcaa64767e9b9c53659e803069c0f50c9d85
icydoge <icydoge@gmail.com>
2016-12-13 23:06:16 -0800

    Day 14.

---
+++ day14.py
@@ -0,0 +1,78 @@
+###################################
+# Such times                      #
+#                  Much effort    #
+#       Very Doge                 #
+###################################
+# By icydoge <icydoge@gmail.com>  #
+###################################
+
+import hashlib
+
+
+class Hasher:
+
+
+    def __init__(self, input_string):
+
+        self.__hash_list = {}
+        self._input_string = input_string
+
+
+    def request(self, current_index, part):
+
+        if current_index in self.__hash_list:
+            return self.__hash_list[current_index]
+
+        else:
+            current_string = self._input_string + str(current_index)
+            current_hash = hashlib.md5(current_string.encode('utf-8')).hexdigest()
+
+            if part == 2:
+                for _ in range(2016):
+                    current_hash = hashlib.md5(current_hash.encode('utf-8')).hexdigest()
+
+            self.__hash_list[current_index] = current_hash
+
+            return current_hash
+
+
+input_string = input("Enter puzzle input: ")
+
+# Part 1, 2.
+for part in [1, 2]:
+
+    hasher = Hasher(input_string)
+    current_index = 0
+    confirmed = []
+
+    while True:
+
+        new_hash = hasher.request(current_index, part)
+        found_current = None
+
+        for i in range(0, len(new_hash) - 2):
+            if new_hash[i] == new_hash[i+1] == new_hash[i+2]:
+                found_current = (new_hash, current_index, new_hash[i])
+                break
+        
+        if found_current is not None:
+            for j in range(1, 1001):
+
+                test_hash = hasher.request(current_index + j, part)
+                test_hash_confirmed = False
+
+                for i in range(0, len(test_hash) - 4):
+                    if found_current[2] == test_hash[i] == test_hash[i+1] == test_hash[i+2] == test_hash[i+3] == test_hash[i+4]:
+                        confirmed.append((new_hash, found_current[1]))
+                        test_hash_confirmed = True
+                        break
+
+                if test_hash_confirmed:
+                    break
+
+        if len(confirmed) >= 64:
+            break
+        
+        current_index += 1
+
+    print("Final answer for Part %d: %d" % (part, confirmed[63][1]))
